12540b880d38c4dd148351ce58022226a15e8ae6
sahil <46827078+sahil-at-home1@users.noreply.github.com>
2022-12-04 21:39:31 -0800

    switched the encoding for my shape choice

--- 2022/day2/src/main.rs
+++ 2022/day2/src/main.rs
@@ -11,9 +11,9 @@ enum Shape {
 impl Shape {
     fn new(lett: String) -> Shape {
         let letter = lett.trim();
-        if letter == "A" || letter == "Y" {
+        if letter == "A" || letter == "X" {
             return Shape::Rock;
-        } else if letter == "B" || letter == "X" {
+        } else if letter == "B" || letter == "Y" {
             return Shape::Paper;
         } else if letter == "C" || letter == "Z" {
             return Shape::Scissors;
@@ -21,11 +21,11 @@ impl Shape {
             panic!("invalid letter: {}", letter);
         }
     }
-    fn points_value(&self) -> i32 {
+    fn points(&self) -> i32 {
         return match self {
             Shape::Rock => 1,
-            Shape::Paper => 1,
-            Shape::Scissors => 1,
+            Shape::Paper => 2,
+            Shape::Scissors => 3,
         };
     }
 }
@@ -37,7 +37,7 @@ enum GameResult {
     Draw,
 }
 impl GameResult {
-    fn points_value(&self) -> i32 {
+    fn points(&self) -> i32 {
         return match self {
             GameResult::Loss => 0,
             GameResult::Draw => 3,
@@ -74,23 +74,29 @@ impl Game {
             result: GameResult::new(op_choice, my_choice),
         }
     }
+    fn points(&self) -> i32 {
+        self.my_choice.points() + self.result.points()
+    }
 }
 
 impl fmt::Display for Game {
     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
-        let points = self.my_choice.points_value() + self.result.points_value();
+        let points = self.my_choice.points() + self.result.points();
         write!(
             f,
-            "{:?} {:?} {:?} {}",
-            self.op_choice, self.my_choice, self.result,
-        );
+            "(Op: {:?}, My: {:?}) {:?} = {} pts.",
+            self.op_choice, self.my_choice, self.result, points,
+        )
     }
 }
 
 fn main() {
-    let contents: String = fs::read_to_string("data/test.txt").unwrap();
+    let contents: String = fs::read_to_string("data/input.txt").unwrap();
     let mut games: Vec<Game> = Vec::new();
     for line in contents.split("\n") {
+        if line.trim() == "" {
+            continue;
+        }
         let mut letters: Vec<String> = line.split(" ").map(String::from).collect();
         let op_choice = Shape::new(letters.pop().unwrap());
         let my_choice = Shape::new(letters.pop().unwrap());
@@ -98,9 +104,7 @@ fn main() {
     }
     let mut total_points: i32 = 0;
     for game in games {
-        total_points += game.my_choice.points_value();
-        total_points += game.result.points_value();
+        total_points += game.points();
     }
-
     println!("Total points: {}", total_points);
 }
