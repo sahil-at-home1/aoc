fb8769dbbeff6d158389970806cc5be626cc4e29
sahil <46827078+sahil-at-home1@users.noreply.github.com>
2022-12-04 16:58:17 -0800

    starting day2

---
+++ 2022/day2/src/main.rs
@@ -0,0 +1,8 @@
+use std::{collections::HashMap, hash::Hash};
+
+const shape_to_point_map: HashMap<&str, i32> =
+    HashMap::from([("Rock", 1), ("Paper", 2), ("Scissors", 3)]);
+
+fn main() {
+    println!("Hello, world!");
+}
