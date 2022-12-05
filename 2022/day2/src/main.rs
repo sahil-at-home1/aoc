0f275fb795949376ff4de74d211c7ba71b896811
sahil <46827078+sahil-at-home1@users.noreply.github.com>
2022-12-04 22:06:01 -0800

    finished day 2

--- 2022/day2/src/main.rs
+++ 2022/day2/src/main.rs
@@ -30,32 +30,45 @@ impl Shape {
     }
 }
 
-#[derive(Debug)]
+#[derive(Clone, Copy, Debug)]
 enum GameResult {
     Win,
     Loss,
     Draw,
 }
 impl GameResult {
+    // fn new(op_choice: Shape, my_choice: Shape) -> GameResult {
+    //     return match (op_choice, my_choice) {
+    //         (Shape::Rock, Shape::Rock) => GameResult::Draw,
+    //         (Shape::Rock, Shape::Paper) => GameResult::Win,
+    //         (Shape::Rock, Shape::Scissors) => GameResult::Loss,
+    //         (Shape::Paper, Shape::Rock) => GameResult::Loss,
+    //         (Shape::Paper, Shape::Paper) => GameResult::Draw,
+    //         (Shape::Paper, Shape::Scissors) => GameResult::Win,
+    //         (Shape::Scissors, Shape::Rock) => GameResult::Win,
+    //         (Shape::Scissors, Shape::Paper) => GameResult::Loss,
+    //         (Shape::Scissors, Shape::Scissors) => GameResult::Draw,
+    //     };
+    // }
+
+    fn new(lett: String) -> GameResult {
+        let letter = lett.trim();
+        if letter == "X" {
+            GameResult::Loss
+        } else if letter == "Y" {
+            GameResult::Draw
+        } else if letter == "Z" {
+            GameResult::Win
+        } else {
+            panic!("invalid letter {}", letter);
+        }
+    }
+
     fn points(&self) -> i32 {
         return match self {
             GameResult::Loss => 0,
             GameResult::Draw => 3,
             GameResult::Win => 6,
-        };
-    }
-
-    fn new(op_choice: Shape, my_choice: Shape) -> GameResult {
-        return match (op_choice, my_choice) {
-            (Shape::Rock, Shape::Rock) => GameResult::Draw,
-            (Shape::Rock, Shape::Paper) => GameResult::Win,
-            (Shape::Rock, Shape::Scissors) => GameResult::Loss,
-            (Shape::Paper, Shape::Rock) => GameResult::Loss,
-            (Shape::Paper, Shape::Paper) => GameResult::Draw,
-            (Shape::Paper, Shape::Scissors) => GameResult::Win,
-            (Shape::Scissors, Shape::Rock) => GameResult::Win,
-            (Shape::Scissors, Shape::Paper) => GameResult::Loss,
-            (Shape::Scissors, Shape::Scissors) => GameResult::Draw,
         };
     }
 }
@@ -67,13 +80,32 @@ struct Game {
 }
 
 impl Game {
-    fn new(op_choice: Shape, my_choice: Shape) -> Game {
+    // fn new(op_choice: Shape, my_choice: Shape) -> Game {
+    //     Game {
+    //         op_choice: op_choice,
+    //         my_choice: my_choice,
+    //         result: GameResult::new(op_choice, my_choice),
+    //     }
+    // }
+    fn new(op_choice: Shape, result: GameResult) -> Game {
+        let my_choice = match (op_choice, result) {
+            (Shape::Rock, GameResult::Loss) => Shape::Scissors,
+            (Shape::Rock, GameResult::Draw) => Shape::Rock,
+            (Shape::Rock, GameResult::Win) => Shape::Paper,
+            (Shape::Paper, GameResult::Loss) => Shape::Rock,
+            (Shape::Paper, GameResult::Draw) => Shape::Paper,
+            (Shape::Paper, GameResult::Win) => Shape::Scissors,
+            (Shape::Scissors, GameResult::Loss) => Shape::Paper,
+            (Shape::Scissors, GameResult::Draw) => Shape::Scissors,
+            (Shape::Scissors, GameResult::Win) => Shape::Rock,
+        };
         Game {
             op_choice: op_choice,
             my_choice: my_choice,
-            result: GameResult::new(op_choice, my_choice),
+            result: result,
         }
     }
+
     fn points(&self) -> i32 {
         self.my_choice.points() + self.result.points()
     }
@@ -98,9 +130,10 @@ fn main() {
             continue;
         }
         let mut letters: Vec<String> = line.split(" ").map(String::from).collect();
-        let my_choice = Shape::new(letters.pop().unwrap());
+        // let my_choice = Shape::new(letters.pop().unwrap());
+        let result = GameResult::new(letters.pop().unwrap());
         let op_choice = Shape::new(letters.pop().unwrap());
-        games.push(Game::new(op_choice, my_choice));
+        games.push(Game::new(op_choice, result));
     }
     let mut total_points: i32 = 0;
     for game in games {
