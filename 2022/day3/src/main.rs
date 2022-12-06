23c073722d3e078663a8a91ad54eaf746b6b3767
sahil <46827078+sahil-at-home1@users.noreply.github.com>
2022-12-05 22:16:06 -0800

    finished day3 part 2

--- 2022/day3/src/main.rs
+++ 2022/day3/src/main.rs
@@ -1,38 +1,40 @@
 use std::fs;
 
-#[derive(Debug)]
-struct Compartment {
-    items: Vec<char>,
-}
+// #[derive(Debug)]
+// struct Compartment {
+//     items: Vec<char>,
+// }
 
-impl Compartment {
-    fn new() -> Compartment {
-        Compartment { items: Vec::new() }
-    }
-}
+// impl Compartment {
+//     fn new() -> Compartment {
+//         Compartment { items: Vec::new() }
+//     }
+// }
 
 #[derive(Debug)]
 struct Rucksack {
-    c1: Compartment,
-    c2: Compartment,
+    // c1: Compartment,
+    // c2: Compartment,
+    items: Vec<char>,
 }
 
 impl Rucksack {
     fn new() -> Rucksack {
         Rucksack {
-            c1: Compartment::new(),
-            c2: Compartment::new(),
+            // c1: Compartment::new(),
+            // c2: Compartment::new(),
+            items: Vec::new(),
         }
     }
 
-    fn find_common_item(&mut self) -> Option<char> {
-        for item in &self.c1.items {
-            if self.c2.items.contains(&item) {
-                return Option::Some(item.clone());
-            }
-        }
-        return Option::None;
-    }
+    // fn find_common_item(&mut self) -> Option<char> {
+    //     for item in &self.c1.items {
+    //         if self.c2.items.contains(&item) {
+    //             return Option::Some(item.clone());
+    //         }
+    //     }
+    //     return Option::None;
+    // }
 }
 
 fn item_to_value(item: char) -> i32 {
@@ -49,26 +51,31 @@ fn item_to_value(item: char) -> i32 {
 
 fn main() {
     let contents: String = fs::read_to_string("data/input.txt").unwrap();
-    // let mut rucksacks: Vec<Rucksack> = Vec::new();
     let mut item_sum: i32 = 0;
 
     // collect all the items into rucksacks
+    const GROUP_SIZE: usize = 3;
+    let mut group: Vec<Rucksack> = Vec::new();
     for line in contents.split("\n") {
         if line.trim() == "" {
             continue;
         }
         let mut rucksack = Rucksack::new();
-        let items: Vec<char> = line.trim().chars().collect();
-        rucksack.c1.items = items[0..(items.len() / 2)].to_vec();
-        rucksack.c2.items = items[(items.len() / 2)..(items.len())].to_vec();
-        // find the common item
-        // println!("{:#?}", rucksack);
-        let common_item: char = rucksack.find_common_item().unwrap();
-        let common_item_val: i32 = item_to_value(common_item);
-        println!("{}: {}", common_item, common_item_val);
-        item_sum += common_item_val;
-        // rucksacks.push(rucksack)
+        rucksack.items.append(&mut line.trim().chars().collect());
+        group.push(rucksack);
+        if group.len() >= GROUP_SIZE {
+            let r1 = &group[0];
+            let r2 = &group[1];
+            let r3 = &group[2];
+            for item in &r1.items {
+                if r2.items.contains(&item) && r3.items.contains(&item) {
+                    item_sum += item_to_value(item.clone());
+                    break;
+                }
+            }
+            // println!("{:?}\n", group);
+            group.clear();
+        }
     }
-
     println!("Sum of common items is {}", item_sum);
 }
