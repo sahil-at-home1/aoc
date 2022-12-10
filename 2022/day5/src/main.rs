2b5272fc5c53617b07ef86c56d90cc9a81cad397
sahil <46827078+sahil-at-home1@users.noreply.github.com>
2022-12-10 12:15:17 -0800

    fixed parsing of items

--- 2022/day5/src/main.rs
+++ 2022/day5/src/main.rs
@@ -6,23 +6,32 @@ fn main() {
     // read the lines backwards
     for line in input.lines().rev() {
         // skip arrange steps, but use stack number step
-        if line.contains("move") || line.is_empty() {
+        if line.contains("move") || line.trim().is_empty() {
             continue;
         } else if line.contains("1") {
-            let nums: Vec<&str> = line.split("\n").collect();
+            let nums: Vec<&str> = line.split_whitespace().collect();
             let num_stacks: usize = nums[nums.len() - 1].parse().unwrap();
             for _ in 0..num_stacks {
                 stacks.push(Vec::new());
             }
+            continue;
         }
         // push items onto stacks
-        let mut stack_num: usize = 0;
-        for word in line.split_whitespace() {
-            let item: char = word.chars().nth(1).unwrap();
-            let stack: &mut Vec<char> = &mut stacks[stack_num];
-            stack.push(item);
-            stack_num += 1
+        let mut stack_idx: usize = 0;
+        let letters: Vec<char> = line.chars().collect();
+        // each item is 3 chars apart
+        for i in 0..letters.len() {
+            let item: char = letters[i];
+            if item.is_alphabetic() {
+                let stack: &mut Vec<char> = &mut stacks[stack_idx];
+                stack.push(item);
+            }
+            // integer division
+            stack_idx = i / 3;
         }
+    }
+    for stack in &stacks {
+        println!("{:?}", stack);
     }
     // move items across stacks based on steps
     for line in input.lines() {
@@ -34,6 +43,8 @@ fn main() {
         let from_idx: usize = words[3].parse::<usize>().unwrap() - 1;
         let to_idx: usize = words[5].parse::<usize>().unwrap() - 1;
         for _ in 0..num_to_move {
+            println!("moving item from {} to {}", from_idx, to_idx);
+
             let item_to_move: char = stacks[from_idx].pop().unwrap();
             stacks[to_idx].push(item_to_move);
         }
@@ -41,6 +52,6 @@ fn main() {
     // print the items on the top of each stack
     print!("The items on the top of each stack are: ");
     for stack in stacks {
-        print!("{}", stack[stack.len()]);
+        print!("{}", stack[stack.len() - 1]);
     }
 }
