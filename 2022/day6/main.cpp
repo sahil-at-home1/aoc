079f17dc08c5275add689d925cf05bba57eb8475
Sahil Sharma <sahilsharm1617@gmail.com>
2023-04-19 12:18:36 -0700

    got first version of part 1 of day6 done

--- 2022/day6/main.cpp
+++ 2022/day6/main.cpp
@@ -1,18 +1,48 @@
 #include <iostream>
 #include <fstream>
 #include <string>
+#include <vector>
 
 using namespace std;
 
 int main()
 {
+    const size_t packet_length = 4;
+    size_t chars_til_packet = 0;
+    size_t valid_char_count = 0;
     string line = "";
     ifstream fin = ifstream("input.txt");
 
-    // read input into a string
-    while (getline(fin, line)) {
-        cout << line << endl;
+    // read input size_to a string
+    while (getline(fin, line))
+    {
+        cout << "read a line." << endl;
     }
-
     fin.close();
+
+    // parse each char of the string
+    for (size_t i = 0; i < line.length(); i++)
+    {
+        if (i < packet_length)
+        {
+            continue;
+        }
+        // check if char is in previous packet chars
+        valid_char_count += 1;
+        for (size_t j = 1; j < (packet_length); j++)
+        {
+            if (line[i] == line[i - j])
+            {
+                valid_char_count = 0;
+                break;
+            }
+        }
+
+        if (valid_char_count >= 4)
+        {
+            chars_til_packet = i;
+            break;
+        }
+    }
+    cout << "Packet was detected after " << chars_til_packet << " characters" << endl;
 }
