5d62efbe2a73d917242b679ef5570831e8f67eb6
Sahil Sharma <sahilsharm1617@gmail.com>
2023-04-20 10:21:30 -0700

    stil debugging

--- 2022/day6/day6.cpp
+++ 2022/day6/day6.cpp
@@ -11,22 +11,26 @@ int get_packet_idx(string line, int packet_length)
     int packet_idx = 0;
     int valid_char_count = 0;
 
-    // parse each char of the string
-    for (int i = 0; i < line.length(); i++)
+    // parse each char of the string with a char behind it
+    for (int i = 1; i < line.length(); i++)
     {
         // check if char is in previous packet chars
         char test_char = line[i];
         valid_char_count += 1;
-        for (int j = 1; j < packet_length; j++)
+        for (int j = 1; j < packet_length || j > i; j++)
         {
-            if ((i - j) < 0)
-            {
-                break;
-            }
+
             char compare_char = line[i - j];
             if (test_char == compare_char)
             {
-                valid_char_count -= packet_length - j;
+                if (i < packet_length)
+                {
+                    valid_char_count -= (i + 2) - j;
+                }
+                else
+                {
+                    valid_char_count -= packet_length - j;
+                }
                 break;
             }
         }
@@ -37,5 +41,5 @@ int get_packet_idx(string line, int packet_length)
             break;
         }
     }
-    return int(packet_idx + 1);
+    return int(packet_idx);
 }
