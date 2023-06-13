856b96a2d99aaf6a87af5097e21e943009682fb7
sahil <46827078+sahil-at-home1@users.noreply.github.com>
2023-06-12 21:41:18 -0700

    got program to read input

--- 2022/day7/main.cpp
+++ 2022/day7/main.cpp
@@ -1,17 +1,38 @@
+#include <fstream>
 #include <iostream>
+#include <string>
 #include <vector>
 
-class MyFile {
+class MyFile
+{
 public:
-  int size;
-  std::string name;
+    int size;
+    std::string name;
 };
 
-class MyDir {
+class MyDir
+{
 public:
-  int size;
-  std::vector<MyFile> files;
-  std::vector<MyDir> dirs;
+    int size;
+    std::vector<MyFile> files;
+    std::vector<MyDir> dirs;
 };
 
-int main() { std::cout << "Hello world!" << std::endl; }
+int main()
+{
+    std::string line;
+    std::ifstream f;
+
+    f.open("day7/input.txt");
+    if (!f.is_open())
+    {
+        std::cout << "could not open file" << std::endl;
+        return 1;
+    }
+
+    while (getline(f, line))
+    {
+        std::cout << line << std::endl;
+    }
+    f.close();
+}
