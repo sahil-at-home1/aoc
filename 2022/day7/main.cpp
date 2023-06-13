2d39454b7a21ced22ae618c7f124a5cbdd20b5a0
sahil <46827078+sahil-at-home1@users.noreply.github.com>
2023-06-12 21:19:25 -0700

    adding classes to make things more organized

--- 2022/day7/main.cpp
+++ 2022/day7/main.cpp
@@ -1,3 +1,17 @@
 #include <iostream>
+#include <vector>
+
+class MyFile {
+public:
+  int size;
+  std::string name;
+};
+
+class MyDir {
+public:
+  int size;
+  std::vector<MyFile> files;
+  std::vector<MyDir> dirs;
+};
 
 int main() { std::cout << "Hello world!" << std::endl; }
