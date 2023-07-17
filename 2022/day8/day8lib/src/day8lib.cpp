c8708cf4752095a86a4f9c1b45dc76f8cf17fe3e
Sahil <sahilsharm1617@gmail.com>
2023-07-17 18:35:13 -0500

    fixed bugs with the sightline calculations

--- 2022/day8/day8lib/src/day8lib.cpp
+++ 2022/day8/day8lib/src/day8lib.cpp
@@ -11,22 +11,18 @@ void day8::check_left_to_right(std::vector<std::vector<day8::Tree *>> *forest,
                                int                                     n) {
     int hStart = 0;
     int hEnd = n;
-    int hIncrement = 1;
+    int hInc = 1;
     int vStart = 0;
     int vEnd = n;
-    int vIncrement = 1;
+    int vInc = 1;
 
     int maxHeight = 0;
-    for (int h = hStart; h != hEnd; h += hIncrement) {
-        for (int v = vStart; v != vEnd; v += vIncrement) {
+    for (int h = hStart; h != hEnd; h += hInc) {
+        for (int v = vStart; v != vEnd; v += vInc) {
             day8::Tree *tree = (*forest)[h][v];
             // check if tree is on edge
-            if (v == vStart) {
-                tree->visibility[day8::Sightline::LeftToRight] = true;
-                continue;
-            }
             // check if tree is taller than all other trees along sightline
-            if (tree->height > maxHeight) {
+            if (v == vStart || tree->height > maxHeight) {
                 tree->visibility[day8::Sightline::LeftToRight] = true;
                 maxHeight = tree->height;
             }
@@ -36,24 +32,22 @@ void day8::check_left_to_right(std::vector<std::vector<day8::Tree *>> *forest,
 
 void day8::check_right_to_left(std::vector<std::vector<day8::Tree *>> *forest,
                                int                                     n) {
-    int hStart = n - 1;
-    int hEnd = -1;
-    int hIncrement = -1;
-    int vStart = 0;
-    int vEnd = n;
-    int vIncrement = 1;
+    int rowStart = 0;
+    int rowEnd = n;
+    int rowInc = 1;
+    // check last col first
+    int colStart = n - 1;
+    int colEnd = -1;
+    int colInc = -1;
 
     int maxHeight = 0;
-    for (int h = hStart; h != hEnd; h += hIncrement) {
-        for (int v = vStart; v != vEnd; v += vIncrement) {
-            day8::Tree *tree = (*forest)[h][v];
+    for (int row = rowStart; row != rowEnd; row += rowInc) {
+        // sweep across columns for each row, from right to left
+        for (int col = colStart; col != colEnd; col += colInc) {
+            day8::Tree *tree = (*forest)[row][col];
             // check if tree is on edge
-            if (v == vStart) {
-                tree->visibility[day8::Sightline::RightToLeft] = true;
-                continue;
-            }
             // check if tree is taller than all other trees along sightline
-            if (tree->height > maxHeight) {
+            if (col == colStart || tree->height > maxHeight) {
                 tree->visibility[day8::Sightline::RightToLeft] = true;
                 maxHeight = tree->height;
             }
@@ -65,22 +59,18 @@ void day8::check_top_to_bot(std::vector<std::vector<day8::Tree *>> *forest,
                             int                                     n) {
     int hStart = 0;
     int hEnd = n;
-    int hIncrement = 1;
+    int hInc = 1;
     int vStart = 0;
     int vEnd = n;
-    int vIncrement = 1;
+    int vInc = 1;
 
     int maxHeight = 0;
-    for (int v = vStart; v != vEnd; v += vIncrement) {
-        for (int h = hStart; h != hEnd; h += hIncrement) {
+    for (int v = vStart; v != vEnd; v += vInc) {
+        for (int h = hStart; h != hEnd; h += hInc) {
             day8::Tree *tree = (*forest)[h][v];
             // check if tree is on edge
-            if (h == hStart) {
-                tree->visibility[day8::Sightline::TopToBot] = true;
-                continue;
-            }
             // check if tree is taller than all other trees along sightline
-            if (tree->height > maxHeight) {
+            if (h == hStart || tree->height > maxHeight) {
                 tree->visibility[day8::Sightline::TopToBot] = true;
                 maxHeight = tree->height;
             }
@@ -90,25 +80,21 @@ void day8::check_top_to_bot(std::vector<std::vector<day8::Tree *>> *forest,
 
 void day8::check_bot_to_top(std::vector<std::vector<day8::Tree *>> *forest,
                             int                                     n) {
-    int hStart = 0;
-    int hEnd = n;
-    int hIncrement = 1;
-    int vStart = n - 1;
-    int vEnd = -1;
-    int vIncrement = -1;
+    int hStart = n - 1;
+    int hEnd = -1;
+    int hInc = -1;
+    int vStart = 0;
+    int vEnd = n;
+    int vInc = 1;
 
     int maxHeight = 0;
-    for (int v = vStart; v != vEnd; v += vIncrement) {
-        for (int h = hStart; h != hEnd; h += hIncrement) {
+    for (int v = vStart; v != vEnd; v += vInc) {
+        for (int h = hStart; h != hEnd; h += hInc) {
             day8::Tree *tree = (*forest)[h][v];
             // check if tree is on edge
-            if (h == hStart) {
-                tree->visibility[day8::Sightline::TopToBot] = true;
-                continue;
-            }
             // check if tree is taller than all other trees along sightline
-            if (tree->height > maxHeight) {
-                tree->visibility[day8::Sightline::TopToBot] = true;
+            if (h == hStart || tree->height > maxHeight) {
+                tree->visibility[day8::Sightline::BotToTop] = true;
                 maxHeight = tree->height;
             }
         }
