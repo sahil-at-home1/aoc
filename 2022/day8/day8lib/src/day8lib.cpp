71689eac2d2dae8abdb5fabc6ec8699e00d4f919
Sahil <sahilsharm1617@gmail.com>
2023-07-19 16:27:08 -0500

    added new scenic factor algorithm

--- 2022/day8/day8lib/src/day8lib.cpp
+++ 2022/day8/day8lib/src/day8lib.cpp
@@ -1,10 +1,11 @@
 #include "day8lib.h"
+#include <algorithm>
 #include <fstream>
 #include <iostream>
+#include <map>
 #include <stdexcept>
 #include <stdio.h>
 #include <string>
-#include <unordered_map>
 #include <vector>
 
 void day8::check_tree_visibilities(day8::Forest *forest) {
@@ -69,28 +70,29 @@ void day8::check_right_to_left(day8::Forest *forest) {
     int  colEnd = -1;
     int  colInc = -1;
 
-    int maxHeight = 0;
-    int maxHeightPos = 0;
     for (int row = rowStart; row != rowEnd; row += rowInc) {
+        std::vector<day8::Tree *> seen;
+        // start sweep
+        int maxHeight = 0;
         for (int col = colStart; col != colEnd; col += colInc) {
             day8::Tree *tree = forest->get_tree(row, col);
-            // check if tree is on edge
-            if (col == colStart) {
-                tree->scenicFactor[sl] = 0;
+            // calculate scenic factor (find distance to first tree with height >= this height)
+            tree->scenicFactor[sl] = 0;
+            for (int i = seen.size() - 1; i > -1; i--) {
+                day8::Tree *seenTree = seen[0];
+                if (seenTree->height >= tree->height) {
+                    tree->scenicFactor[sl] = i + 1;
+                }
             }
-            // check if tree is taller than all other trees along sightline
+            // check visibility
             if (col == colStart || tree->height > maxHeight) {
-                // calculate partial visibility
                 tree->visibility[sl] = true;
                 maxHeight = tree->height;
-                // calculate scenic factor
-                tree->scenicFactor[sl] = col - maxHeightPos;
-                if (tree->scenicFactor[sl] < 0) {
-                    tree->scenicFactor[sl] = tree->scenicFactor[sl] * -1;
-                }
-                maxHeightPos = col;
             }
+            seen.push_back(tree);
         }
+        // reuse buffer for each sweep
+        seen.clear();
     }
 }
 
