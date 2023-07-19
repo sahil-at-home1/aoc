5909f997870f02366d71e53999f55eae5afaca0b
Sahil <sahilsharm1617@gmail.com>
2023-07-19 14:14:12 -0500

    got mem dump to work in debug mode in visual studio

--- 2022/day8/src/main.cpp
+++ 2022/day8/src/main.cpp
@@ -15,20 +15,18 @@ const std::string inputFile =
     "C:/users/sahil/dev/aoc/2022/day8/input/large.txt";
 
 int main() {
-    _CrtMemState s1;
+    _CrtSetReportMode(_CRT_WARN, _CRTDBG_MODE_DEBUG);
 
     day8::Forest *forest = new day8::Forest(inputFile);
     day8::check_tree_visibilities(forest);
-
-    _CrtMemCheckpoint(&s1);
 
     // std::cout << *forest << std::endl;
     std::cout << "There are " << forest->get_num_trees_visible()
               << " visible trees" << std::endl;
 
+    _CrtDumpMemoryLeaks();
+
     delete forest;
 
-    _CrtSetReportMode(_CRT_WARN, _CRTDBG_MODE_DEBUG);
     _CrtDumpMemoryLeaks();
-    _CrtMemDumpStatistics(&s1);
 }
